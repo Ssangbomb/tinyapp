536b8f17412efa64d2d74bfb35dc4336260f041d
ssangbomb <ssangbomb@gmail.com>
2023-04-17 23:09:14 -0700

    Done with URL Shortening Part 1

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
-app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {};
+
 
 const express = require("express");
 const app = express();
@@ -10,6 +11,13 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
